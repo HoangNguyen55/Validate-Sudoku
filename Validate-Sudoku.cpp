a5eaa5806d5370406b59ac1924e4324fa56dcb58
Hoang Nguyen <kqn5278@psu.edu>
2021-12-01 09:48:39 -0500

    paste in check sudoku row program.

--- Validate-Sudoku.cpp
+++ Validate-Sudoku.cpp
@@ -1,20 +1,70 @@
 // Validate-Sudoku.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Hoang Nguyen - Cmpsc 122
+// check if sudoku puzzle is valid
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+std::string check(std::vector<int>&);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string out;
+    std::vector<int> rowsToTest[] = {
+      {2, 2, 2, 5, 47, 7, 8, 9, 10},
+      {0, 0, 0, 0},
+      {1, 5, 6, 7, 8, 4, 2, 3, 9},
+      {1, 1, 2, 4, 7, 2},
+      {9, 6, 5, 4, 7, 2, 3, 4, 4, 18},
+      {100, 2636, 24, 288, 3, 9, 7, 89},
+      {98, 1, 3, 0, 7, 5, 9, 7, 2, 3},
+      {1, 5, 6, 7, 8, 69, 73, 3, 69},
+    };
+
+    for (std::vector<int> row : rowsToTest)
+    {
+        out = "\nCurrent testing row: [";
+        for (int val : row)
+            out += " " + std::to_string(val) + ",";
+        out.pop_back();
+        std::cout << out << " ]\n";
+        std::cout << check(row) << std::endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+std::string check(std::vector<int> &arr)
+{
+    std::string err = "";
+    std::vector<int> appeared = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    bool happened = false;
+    bool outOfRange = false;
+    int x;
+    if (arr.size() != 9)
+        err += "The length of the row is not 9\n";
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (int i = 0; i < arr.size(); i++)
+    {
+        x = arr[i];
+        if ((x < 1 || x > 9) && !outOfRange)
+        {
+            err += "There is a number that is out of range from 1 to 9\n";
+            outOfRange = true;
+        }
+        else if (x > 0 && x < 9)
+        {
+            if (appeared[x - 1] == x && !happened)
+            {
+                err += "There is a number in range that is repeated\n";
+                happened = true;
+            }
+            else
+                appeared[x - 1] = x;
+        }
+    }
+
+    if (!err.length())
+        err = "The row is valid";
+
+    return err;
+}
